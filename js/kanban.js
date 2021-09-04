364432b41d3874b32ff8307ba3eaa424913852c3
Kyle Calder <kylebcalder03@gmail.com>
2021-09-03 23:36:11 -0600

    added save text

--- js/kanban.js
+++ js/kanban.js
@@ -25,6 +25,7 @@ const create_item = () => {
   let input = document.createElement('input');
   item.appendChild(input);
   let save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
 };
 
 document.querySelectorAll('.drop').forEach(element => {
